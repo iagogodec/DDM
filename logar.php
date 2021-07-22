b8a6c82fb4e74d6226c340786397ad96673246cb
iago godec <64555551+iagogodec@users.noreply.github.com>
2021-07-22 19:44:50 -0300

    Add files via upload

---
+++ logar.php
@@ -0,0 +1,15 @@
+<?php
+    include_once 'conexao.php';
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    //executar uma pesquisa no bd
+    $sql = $dbcon->query("SELECT * FROM tblogin WHERE email='$email' AND senha='$senha'");
+
+    if(mysqli_num_rows($sql)>0){
+        echo "login_ok";
+    }else{
+        echo "login_erro";
+    }
+?>
