7aa8c251f9b4a497ed04928e5ddf992eb1311a71
iago godec <64555551+iagogodec@users.noreply.github.com>
2021-08-18 08:54:26 -0300

    Add files via upload

--- registrar.php
+++ registrar.php
@@ -1,23 +1,26 @@
 <?php
-    incrude_once 'conexao.php'
+include_once 'conexao.php';
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$nome = $_POST['nome'];
 
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    $nome = $_POST['nome'];
 
-    $sql2 = $dbcon->query("SELECT * FROM tblogin WHERE email='$email' AND senha='$senha'")
+$sql2 = $dbcon->query("SELECT * FROM tblogin where email='$email' ");
 
-    if(mysqli_num_rows($sql2)>0){
-        echo "email_erro";
-    }else{
-        //echo "vai continuar...";
-        //vamos deixar fazer o registro -> INSERT
-        $sql3 = $dbcon->query("INSERT INTO tblogin(nome,email,senha) VALUES ('$nome','$email','$senha')");
+if(mysqli_num_rows($sql2)>0){
+    echo "email erro";
+}else{
+$sql3 = $dbcon->query("INSERT INTO tblgin('nome','email','senha') VALUES('$nome','$email','$senha')");
+}
 
-        if($sql3){
-            echo "registro_ok";
-        }else{
-            echo "registro_erro";
-        }
-    }
+
+if($sql3){
+    echo "registro ok";
+}else{
+ echo "registro erro";
+}
+
+
+
+
 ?>
